085a36e9e75b1b0348a7a79ee4f2bba65b36a61c
Harold Jean <haroldjean38@yahoo.com>
2022-11-16 15:34:51 -0500

    added submit eventlistener fir signupform

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const signInForm = document.getElementById('sign-in');
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit' async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
