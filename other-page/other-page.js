1132c1dba62e11a1ef90250612b07f86297792c3
Harold Jean <haroldjean38@yahoo.com>
2022-11-16 18:01:36 -0500

    checkAuth and logout added

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,4 +1,12 @@
 // Import
 import { checkAuth, logout } from '../fetch-utils.js';
+checkAuth();
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
